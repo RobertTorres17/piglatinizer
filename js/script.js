850cb85c3c429923e7af35b3dad9999c84422768
RobertTorres17 <ajanitorres1@gmail.com>
2019-10-23 22:16:12 +0000

    work in progress on vowel declaringc

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
 
-
-
-  
-  
-  
-  
 });
+    function checkVowel(input){
+        // Checks the vowels
+    input = input.toLowerCase();
+    let vowels = ["a","i","e","o","u"]
+
+    }
