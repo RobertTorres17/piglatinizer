118bff4229603d61963c44be83695d58292ce663
RobertTorres17 <ajanitorres1@gmail.com>
2019-11-06 23:23:39 +0000

    Started the If Else Statement

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+    $("#button").click(function() {
+     let word =  $(".input").val();
+    $(".output").text(translate(word) );
 
-});
+};
+function translate(word){
+    if(checkVowel(word) ) {
+        return( word + "ay")
+    }
+    
+
+}
     function checkVowel(input){
         // Checks the vowels
     input = input.toLowerCase();
     let vowels = ["a","i","e","o","u"]
 
     }
+
